a7b2ad025353ebdce7e081f03b57f6f9b8847144
Mika215 Michael <mikalsoyra@gmail.com>
2021-09-18 18:38:12 +0200

    adding js volume control

---
+++ init.js
@@ -0,0 +1,6 @@
+/**
+ * I am using java script because i couldn't find any other means to control the volume of the background audio for my game
+ */
+
+ let audio = document.getElementById("player");
+ audio.volume = 0.02;
